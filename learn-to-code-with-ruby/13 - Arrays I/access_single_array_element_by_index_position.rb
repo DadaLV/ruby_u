c8c3882fefba8979ee5218b0ef9287a54946a1f6
DadaLV <vynohradova.l.a@gmail.com>
2023-01-06 01:31:14 +0200

    ruby_u

---
+++ learn-to-code-with-ruby/13 - Arrays I/access_single_array_element_by_index_position.rb
@@ -0,0 +1,17 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[3]
+p fruits[100]
+p fruits[4]
+
+puts fruits[-1]
+puts fruits[-2]
+puts fruits[-4]
+p fruits[-100]
+
+
+puts fruits.[](0)
+puts fruits.[](-1)
+p fruits.[](-15)
