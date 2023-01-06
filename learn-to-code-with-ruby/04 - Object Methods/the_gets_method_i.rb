9948d1f9ee3b8d9645ed2a8336aa3c47421874a9
DadaLV <vynohradova.l.a@gmail.com>
2023-01-07 00:41:48 +0200

    boolean

---
+++ learn-to-code-with-ruby/04 - Object Methods/the_gets_method_i.rb
@@ -0,0 +1,6 @@
+puts "Hi, what's your name?"
+name = gets.chomp
+puts "Great, whats youe age?"
+age = gets.chomp.to_i
+
+puts "Cool, so your name is #{name} and you are #{age} years old"
---
+++ learn-to-code-with-ruby/05 - Numbers and Booleans/intro_to_booleans.rb
@@ -0,0 +1,7 @@
+p 5 < 10
+age = 25
+handsome = true
+stupid = false
+
+p handsome
+p stupid.class 
