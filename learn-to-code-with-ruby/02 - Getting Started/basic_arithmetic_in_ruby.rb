4245991f56730c6c4c2c3044bf9608d7c6f5dcad
DadaLV <vynohradova.l.a@gmail.com>
2023-01-06 03:07:21 +0200

    add arithm methods

---
+++ learn-to-code-with-ruby/02 - Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,20 @@
+puts 1 + 3
+puts 3 + 3 * 5
+
+#PEMDAS
+# Parentheses, Exponents, Multiplication, Division, Addition, Substraction
+
+puts 10 / 5
+puts 12 / 5
+
+puts 12.0 / 5
+puts 12 / 5.0
+puts 12.0 / 5.0
+
+puts 0.5 / 2
+
+puts 5 % 2
+
+puts 6 % 2
+
+
