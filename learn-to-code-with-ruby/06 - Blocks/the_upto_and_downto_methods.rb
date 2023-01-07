852ec107c3f195f7861d4b6e2a85120044e9d627
DadaLV <vynohradova.l.a@gmail.com>
2023-01-07 02:37:25 +0200

    upto downto

---
+++ learn-to-code-with-ruby/06 - Blocks/the_upto_and_downto_methods.rb
@@ -0,0 +1,16 @@
+# 5.downto(1) { |i| puts "Countdown: #{i}" }
+# 5.downto(0) do |current_number|
+#   puts "We are currently on #{current_number}"
+#   puts "hooray"
+# end
+
+# puts "liftoff"
+
+6.upto(10) do |num|
+  puts "we are up"
+  puts "we are on #{num}"
+end
+
+2.upto(7) do
+  puts "we are going up!"
+end
