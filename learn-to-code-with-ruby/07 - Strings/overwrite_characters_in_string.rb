e333994b4127e626f4688f0786f7523507f848ce
DadaLV <vynohradova.l.a@gmail.com>
2023-01-08 02:41:42 +0200

    overriding characters in the string

---
+++ learn-to-code-with-ruby/07 - Strings/overwrite_characters_in_string.rb
@@ -0,0 +1,10 @@
+thing = "rocket ship"
+thing[0] = "p"
+p thing
+
+fact = "I love blueberry pie"
+p fact[7, 4] = "rasp"
+p fact #"I love raspberry pie"
+
+fact[7..10] = "blue"
+p fact #"I love blueberry pie"
