51c8c069a2894522bb03db74e965cf05cb0fb96e
DadaLV <vynohradova.l.a@gmail.com>
2023-01-08 02:49:59 +0200

    uppercase/lowercases/swapcase

---
+++ learn-to-code-with-ruby/07 - Strings/case_methods.rb
@@ -0,0 +1,10 @@
+puts "hello".capitalize #Hello
+puts "heLLo".capitalize #Hello
+puts "hello word".capitalize #Hello word
+puts "hello Word".capitalize #Hello word
+
+puts "liuba123".upcase #LIUBA123
+
+puts "LIUBA#$^&".downcase #liuba#$^&
+
+puts "LiuBa".swapcase #lIUbA
