876c605e8e78f26045d2dc2b2575cb1be128ce4e
DadaLV <vynohradova.l.a@gmail.com>
2023-01-08 02:28:24 +0200

    mulyu slice

---
+++ learn-to-code-with-ruby/07 - Strings/extract_multiple_characters_from_a_string.rb
@@ -0,0 +1,9 @@
+story = "Once upon a time in a land far, far away..."
+
+p story[5, 4] #starting from 5th 4 character = upon
+p story.slice(5, 4) #starting from 5th 4 character = upon
+
+p story[0, story.length]
+
+p story[-7, 5] #away
+p story.sclice(-7, 5) #away
