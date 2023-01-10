6e3a6b40b804f271a239e88750259b24ac2640a1
DadaLV <vynohradova.l.a@gmail.com>
2023-01-11 00:26:15 +0200

    puts print return

---
+++ learn-to-code-with-ruby/08 - Methods I/return_values_ii_implicit_return_values.rb
@@ -0,0 +1,17 @@
+def nothing
+  
+end
+
+p nothing #nil
+
+def return_string
+  "What will be return here?"
+end
+
+def return_guess
+  puts "What will be the return here?"
+end
+
+p return_string #"What will be return here?"
+
+p return_guess # What will be the return here? nil
