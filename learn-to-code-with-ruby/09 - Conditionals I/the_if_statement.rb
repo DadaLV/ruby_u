52eb6c8453cd0d40e99e40a9b5fa855c7dccf075
DadaLV <vynohradova.l.a@gmail.com>
2023-01-11 00:59:02 +0200

    if statement

---
+++ learn-to-code-with-ruby/09 - Conditionals I/the_if_statement.rb
@@ -0,0 +1,22 @@
+if 5 < 7
+  puts "That math statement is true!"
+end
+
+password = "topsecret"
+if password == "topsecret"
+  puts "You are logged in"
+end
+
+word = "kangoroo"
+
+if word.length == 8
+  puts "That word has 8 letters"
+end
+
+if word.include?("eb")
+  puts "Yes, your word includes eb"
+end
+
+if 5.odd?
+  puts "That number is odd"
+end
