db3c1be0e8486607e017717221e821bedbc9f6de
DadaLV <vynohradova.l.a@gmail.com>
2023-01-11 16:02:51 +0200

    or

--- learn-to-code-with-ruby/09 - Conditionals I/multiple_conditions_with_&&_(and)_operator.rb
+++ learn-to-code-with-ruby/09 - Conditionals I/multiple_conditions_with_&&_(and)_operator.rb
@@ -6,3 +6,11 @@ if age > 21 && ticket == "General Admission" && id #we don't need to write id ==
   puts "Congratulation, welcome to the show"
 
 end
+
+budget = 5
+price = 10
+mood = "happy"
+
+if budget > price || mood == "happy"
+  puts "I'm going to buy the item!"
+end
