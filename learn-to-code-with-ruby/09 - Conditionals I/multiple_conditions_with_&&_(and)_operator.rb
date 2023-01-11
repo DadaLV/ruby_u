216120f179d5974829b37e6fac88836bf614998c
DadaLV <vynohradova.l.a@gmail.com>
2023-01-11 15:58:27 +0200

    multy condition

---
+++ learn-to-code-with-ruby/09 - Conditionals I/multiple_conditions_with_&&_(and)_operator.rb
@@ -0,0 +1,8 @@
+age = 25
+ticket = "General Admission"
+id = true
+
+if age > 21 && ticket == "General Admission" && id #we don't need to write id == true because id = true
+  puts "Congratulation, welcome to the show"
+
+end
