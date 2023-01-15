565f4caaf9020b923d9e321a3a596b2c98a2c5b4
DadaLV <vynohradova.l.a@gmail.com>
2023-01-15 03:01:26 +0100

    array

---
+++ learn-to-code-with-ruby/13 - Arrays I/create_an_array_with_square_bracket_syntax.rb
@@ -0,0 +1,9 @@
+numbers = [4, 8, 15, 16, 23, 42]
+
+toys = ["Teddy bear", "Super Soaker", "board game"]
+
+things = [4, true, "Hello", 10.99]
+
+registrations = [true, true, false]
+
+students = [["Liuba", 25, true], ["Sally", 23, true]]
