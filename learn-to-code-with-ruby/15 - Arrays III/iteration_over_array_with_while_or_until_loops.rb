45dfc23ec8c6c42a50cf41053ae5477fa495c8d1
DadaLV <vynohradova.l.a@gmail.com>
2023-01-16 01:39:55 +0200

    until while loop

---
+++ learn-to-code-with-ruby/15 - Arrays III/iteration_over_array_with_while_or_until_loops.rb
@@ -0,0 +1,15 @@
+animals = ["lion", "zebra", "baboon", "cat"]
+
+# i = 0
+# while i < animals.length
+#   puts i
+#   puts animals[i]
+#   i += 1
+# end
+
+i = 0
+until i >= animals.length
+  puts i
+  puts animals[i]
+  i += 1
+end
