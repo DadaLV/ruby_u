c3d3b331baa9bcb21d0c941c44567640e6b22e15
DadaLV <vynohradova.l.a@gmail.com>
2023-01-15 14:22:52 +0200

    fetch method

--- learn-to-code-with-ruby/13 - Arrays I/access_single_array_element_with_the_fetch_method.rb
+++ learn-to-code-with-ruby/13 - Arrays I/access_single_array_element_with_the_fetch_method.rb
@@ -6,3 +6,9 @@ p fruits.[](0)
 p fruits[7] #nil
 p fruits[fruits.length - 1] #last position
 p fruits[-1] #last element
+
+names = ["Tom", "Jim", "Bob"]
+p names.fetch(2) #Bob
+p names.fetch(100) #error
+p names.fetch(100, nil) #if the position doesn't exists it returns the second argument (nil)
+p names.fetch(55, ["A", "B"]) # ["A", "B"]
