c75bd64389116fd8da7d7c494586db2c7be4f623
DadaLV <vynohradova.l.a@gmail.com>
2023-01-15 02:51:15 +0100

    rand method

---
+++ learn-to-code-with-ruby/12 - Ranges/random_numbers_in_ruby.rb
@@ -0,0 +1,5 @@
+puts rand #floating number between 0 and 1
+puts rand.round(2) #floating number between 0 and 1 with 2 numbers after comma
+puts rand(10) #random numbet to 10 but not included 0...10
+puts rand * 100 #randind floating number
+puts rand(50..60)
