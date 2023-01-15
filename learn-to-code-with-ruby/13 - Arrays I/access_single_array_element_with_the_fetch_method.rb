a59d2c91e0bd21cfccb4632fb1347488aa2ff51c
DadaLV <vynohradova.l.a@gmail.com>
2023-01-15 03:17:18 +0100

    position

---
+++ learn-to-code-with-ruby/13 - Arrays I/access_single_array_element_with_the_fetch_method.rb
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+
+p fruits.length #4
+p fruits[0] #extracts apple "Apple" or
+p fruits.[](0)
+p fruits[7] #nil
+p fruits[fruits.length - 1] #last position
+p fruits[-1] #last element
