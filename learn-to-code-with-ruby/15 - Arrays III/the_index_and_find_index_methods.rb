5af2d4ca5a11510a5c8c54c06eefe64df5bb5478
DadaLV <vynohradova.l.a@gmail.com>
2023-01-16 01:03:25 +0200

    challenge

--- learn-to-code-with-ruby/15 - Arrays III/the_index_and_find_index_methods.rb
+++ learn-to-code-with-ruby/15 - Arrays III/the_index_and_find_index_methods.rb
@@ -1,16 +1,43 @@
-colors = ["red", "blue", "green", "yellow"]
+# colors = ["red", "blue", "green", "yellow"]
 
-colors.each_with_index do |color, index|
-  puts "Moving on to index number #{index}"
-  puts "The current color is #{color}"
+# colors.each_with_index do |color, index|
+#   puts "Moving on to index number #{index}"
+#   puts "The current color is #{color}"
+# end
+
+# fives = [5, 10, 15, 20, 25]
+
+# #whrite a loop that iterates over a numeric array
+# # Output the product of each number and its index position
+
+# fives.each_with_index do |number, i|
+#   puts "The curent value is #{number} at index #{i}!"
+#   puts number * i   
+# end
+
+#[1, 2, 3, 4, 5]
+# write a loop that gives me a number of the multiplication values of products of each index and its value
+
+array = [1, 2, 3, 4, 5]
+sum = 0
+array.each_with_index do |num, index|
+  result = num * index
+  sum += result  
 end
 
-fives = [5, 10, 15, 20, 25]
+p sum
 
-#whrite a loop that iterates over a numeric array
-# Output the product of each number and its index position
+# prints the product of the element and its index position if the index position is greater than element
+#create this within a method
 
-fives.each_with_index do |number, i|
-  puts "The curent value is #{number} at index #{i}!"
-  puts number * i   
+def print_if(array)
+  array.each_with_index do |element, index|
+    if index > element
+      puts "Element: #{element} is on position #{index}"
+      puts "Result of multiplying them is #{element * index}"
+    end 
+  end
 end
+arr = [-1, 2, 1, 2, 5, 7, 3]
+
+print_if(arr)
