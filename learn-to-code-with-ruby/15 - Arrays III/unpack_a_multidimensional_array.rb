da3a8909e0beb79877d41d6d02fd21a45ed31e76
DadaLV <vynohradova.l.a@gmail.com>
2023-01-16 02:48:27 +0200

    unpack

---
+++ learn-to-code-with-ruby/15 - Arrays III/unpack_a_multidimensional_array.rb
@@ -0,0 +1,8 @@
+users = [["Bob", 25, "male"], ["Susan", 48, "female"], ["Larry", 25, "male"]]
+
+p users[1][2] # female
+
+bob, susan, larry = users
+p bob # ["Bob", 25, "male"]
+p susan # ["Susan", 48, "female"]
+p larry # ["Larry", 25, "male"]
