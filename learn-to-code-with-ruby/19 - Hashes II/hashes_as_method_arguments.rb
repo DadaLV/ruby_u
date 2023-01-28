187251fec3a58387832cd82982c2138ff46864bc
DadaLV <vynohradova.l.a@gmail.com>
2023-01-28 23:21:32 +0200

    hashes as a method arguments

---
+++ learn-to-code-with-ruby/19 - Hashes II/hashes_as_method_arguments.rb
@@ -0,0 +1,16 @@
+def calculate_total_1(price, tip, tax)
+  tax_amount = price * tax
+  tip_amount = price * tip
+  price + tip_amount + tax_amount
+end
+
+puts calculate_total_1(24.99, 0.18, 0.07)
+
+def calculate_total_2(info)
+  tax_amount = info[:price] * info[:tax]
+  tip_amount = info[:price] * info[:tip]
+  info[:price] + tip_amount + tax_amount
+end
+
+bill = {price: 74.99, tip: 0.18, tax: 0.05}
+puts calculate_total_2(bill)
