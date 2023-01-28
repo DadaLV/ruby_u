bd9c25bd4a62b81aee9596e6a381dfa86a6e95fa
DadaLV <vynohradova.l.a@gmail.com>
2023-01-28 23:59:51 +0200

    delete method on hash

---
+++ learn-to-code-with-ruby/19 - Hashes II/the_delete_method.rb
@@ -0,0 +1,5 @@
+superheroes = {spiderman: "Peter Parker", superman: "Clark Kent", batman: "Bruce Wayne"}
+
+p removed = superheroes.delete(:spiderman) # "Peter Parker"
+p superheroes # {:superman=>"Clark Kent", :batman=>"Bruce Wayne"}
+
