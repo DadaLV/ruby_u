1e76d4dc4c135218cefbe87048314b61d198bb16
DadaLV <vynohradova.l.a@gmail.com>
2023-01-28 13:17:14 +0200

    extract keys and values as an array

---
+++ learn-to-code-with-ruby/18 - Hashes I/retrieve_keys_or_values_from_hash_as_array.rb
@@ -0,0 +1,5 @@
+shopping_list = {bananas: 5, oranges: 10, carrots: 3, crackers: 5}
+
+p shopping_list.keys # [:bananas, :oranges, :carrots, :crackers]
+p shopping_list.values # [5, 10, 3, 5]
+p shopping_list.values.uniq # [5, 10, 3]
