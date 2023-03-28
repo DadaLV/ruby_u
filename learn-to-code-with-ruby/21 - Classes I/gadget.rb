2374e34657e4bb553d387817b87722237fb46eb8
DadaLV <vynohradova.l.a@gmail.com>
2023-03-29 00:37:02 +0200

    instance method

--- learn-to-code-with-ruby/21 - Classes I/gadget.rb
+++ learn-to-code-with-ruby/21 - Classes I/gadget.rb
@@ -1,7 +1,19 @@
 class Gadget
-  
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+  def info
+    "Gadget #{@production_number} has the username #{@username}"
+  end
 end
 
-puts phone = Gadget.new 
-puts laptop = Gadget.new 
-puts microwave = Gadget.new 
+
+
+phone = Gadget.new 
+laptop = Gadget.new 
+puts phone.info
+puts laptop.info
+puts phone.methods - Object.methods
