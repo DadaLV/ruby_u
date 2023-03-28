35ac2b68efc81fb983975abf2764cbd7808cf99c
DadaLV <vynohradova.l.a@gmail.com>
2023-03-29 00:56:47 +0200

    stance variables

---
+++ learn-to-code-with-ruby/21 - Classes I/instance_variables_and_the_initialize_method.rb
@@ -0,0 +1,8 @@
+class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+end
