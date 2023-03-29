b51a31b941d9d60f0b64f96f83b7459bc0df590d
DadaLV <vynohradova.l.a@gmail.com>
2023-03-29 22:58:36 +0200

    modules with identical methods

---
+++ learn-to-code-with-ruby/24 - Modules and Mixins/modules_with_identical_methods.rb
@@ -0,0 +1,23 @@
+module Square
+  def self.area(side)
+    side * side
+  end
+end
+
+module Rectangle
+  def self.area(length, width)
+    length * width
+  end
+end
+
+module Circle
+  PI = 3.14159
+
+  def self.area(radius)
+    PI * radius * radius
+  end
+end
+
+puts Square.area(10)
+puts Rectangle.area(2, 5)
+puts Circle.area(15)
