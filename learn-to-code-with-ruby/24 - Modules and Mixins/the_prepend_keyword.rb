96fe4c5e1c8189718a6a8d8fb34a2cddda8d1644
DadaLV <vynohradova.l.a@gmail.com>
2023-03-30 18:58:18 +0200

    prepend keyword

---
+++ learn-to-code-with-ruby/24 - Modules and Mixins/the_prepend_keyword.rb
@@ -0,0 +1,17 @@
+module Purchaseable
+  def purchase(item)
+    "#{item} has been purchased!"
+  end
+end
+
+class Bookstore
+  prepend Purchaseable
+
+  def purchase(item)
+    "You bought a copy of #{item} at the bookstore!"
+  end
+end
+
+p Bookstore.ancestors #[Purchaseable, Bookstore, Object, Kernel, BasicObject]
+bn = Bookstore.new  
+p bn.purchase("1984")
