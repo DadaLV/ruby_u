b3cc967180753db22316a4ed74f140c28a913950
DadaLV <vynohradova.l.a@gmail.com>
2023-03-30 21:52:22 +0200

    add validation to setter method

---
+++ learn-to-code-with-ruby/22 - Classes II/add_validation_to_setter_methods.rb
@@ -0,0 +1,41 @@
+class Gadget
+  
+  attr_reader :production_number
+  attr_accessor :username
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @production_number = generate_production_number
+  end
+
+  def to_s
+    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it had th ID #{self.object_id}."
+  end
+
+  def password=(new_password)
+    @password = new_password if validate_password(new_password)      
+  end
+
+  private  
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."Z").to_a
+    middle_digits = "2023"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~ /\d/
+  end
+  
+end
+
+phone = Gadget.new("user1", "password1")
+p phone.password 
+
+phone.password = "123tytyt"
+puts phone.password  
