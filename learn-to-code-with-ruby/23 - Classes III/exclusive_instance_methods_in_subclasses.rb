c5b47711c0fc5549592b6ea31ac41dd2763ca4bd
DadaLV <vynohradova.l.a@gmail.com>
2023-03-31 18:32:34 +0200

    exclusive methods in subclasses

---
+++ learn-to-code-with-ruby/23 - Classes III/exclusive_instance_methods_in_subclasses.rb
@@ -0,0 +1,42 @@
+class Employee
+  
+  attr_accessor :age
+  attr_reader :name
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    "Hi, my name is #{name} and I am #{age} years old"
+  end
+end
+
+boris = Employee.new("Boris", 25) 
+
+class Manager < Employee
+  def yell
+    "Who's the boss? I'm the boss!"
+  end
+end
+
+class Worker < Employee
+  def clock_in(time)
+    "Starting my shift at #{time}"
+  end
+
+  def yell
+    "I'm working! I'm working!"
+  end
+end
+
+bob = Manager.new("Bob", 48)
+dan = Worker.new("Dan", 36)
+
+p bob.introduce  
+p dan.introduce  
+
+p dan.yell  
+p bob.yell  
+p dan.clock_in(17)
